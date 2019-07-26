e77d815604ba1a29d41c7f6570c32e28a6eb0639
Shivansh Mittal <shivanshmittl77@gmail.com>
2019-07-26 16:48:06 +0530

    adding changes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Mac-environement-Dot-Files-
 
 ## setup.sh
+* For autoupdates to work in all apps via brew cask remember to remove apple quarantite flag for all applications by running -  sudo xattr -d -r com.apple.quarantine /Applications/*
+	* Apple add quarantite flag to all application downladed and moved using terminal coomand to applications folder, as we are installing app using brew so all application casks will e marked as quarantine and application will run from reaad only apptranslocation folder. so after installing application thriugh cask remmeber to clear qurantine flag which is automatically cleared when we move using finder on mac os. [link](https://lapcatsoftware.com/articles/app-translocation.html)
+
 
 ## Last but not the least
 * don't forget to install dark solarized theme in iterm
@@ -12,3 +15,8 @@
 * Boot OS X into Recovery Mode: hold down the command + R keys
 * choose Terminal then type csrutil disable; reboot
 * csrutil enable;reboot to revert back to more secure mac.
+
+## TODOS ater setup
+* enable google calendar notifications visiting them.
+
+
--- cask_stack.sh
+++ cask_stack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#setting environmentto stop homebrew autoupdating each time when installing cask for this session only
+#setting environment to stop homebrew autoupdating each time when installing cask for this session only
 export HOMEBREW_NO_AUTO_UPDATE=1
 
 brew cask install java8
@@ -22,6 +22,7 @@ brew cask install pycharm-ce
 brew cask install intellij-idea
 
 brew cask install nvalt
+#many of my notes are on simplenote cloud.
 brew cask install simplenote
 brew cask install postman
 
@@ -36,6 +37,20 @@ brew cask install charles
 #for cleaning app caches and all junks of app, its a paid app
 #brew cask install cleanmymac
 
-brew cask install dash
+#I have license for dash 2 of abhishek sengar so download dash2.
+#brew cask install dash
+brew cask install dash2
 brew cask install alfred
 brew cask install visual-studio-code
+
+#for memoizing the knowledge, its the best research technique that should be incooperated in our learning process.
+brew cask install anki
+
+#join active dev community channels on slack for updates.
+brew cask install slack
+
+#better window management with mutple screens using keyboard shortcuts
+#brew cask install spectacle
+
+sudo xattr -d -r com.apple.quarantine /Applications/*
+
